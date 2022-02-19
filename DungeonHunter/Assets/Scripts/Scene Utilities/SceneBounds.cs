416e08f6037765e6d027ff6bb61ef9f70c824097
Sundrop <netherchef@gmail.com>
2022-02-19 13:55:16 +0800

    Implement [Wall Positioning]
    The Scenebounds script can now Automatically Position Walls.

--- DungeonHunter/Assets/Scripts/Scene Utilities/SceneBounds.cs
+++ DungeonHunter/Assets/Scripts/Scene Utilities/SceneBounds.cs
@@ -15,6 +15,17 @@ public class SceneBounds : MonoBehaviour
 	[SerializeField]
 	private int height = 10;
 
+	[Header("Walls:")]
+
+	[SerializeField]
+	private Transform topWall;
+	[SerializeField]
+	private Transform btmWall;
+	[SerializeField]
+	private Transform leftWall;
+	[SerializeField]
+	private Transform rightWall;
+
 	[Header ("Debug:")]
 
 	public bool debug;
@@ -24,6 +35,17 @@ public class SceneBounds : MonoBehaviour
 	//	if (_instance != null && _instance != this) Destroy (this);
 	//	else _instance = this;
 	//}
+
+	//!!! TEMPORARY !!!
+	private void Start()
+	{
+		Prep ();
+	}
+
+	public void Prep ()
+	{
+		PrepWalls ();
+	}
 
 #if UNITY_EDITOR
 	private void Update ()
@@ -74,4 +96,71 @@ public class SceneBounds : MonoBehaviour
 	public Vector3 BtmRight () { return new Vector3 (width / 2, -height / 2, 0); }
 	public Vector3 BtmLeft () { return new Vector3 (-width / 2, -height / 2, 0); }
 	public Vector3 TopLeft () { return new Vector3 (-width / 2, height / 2, 0); }
+
+	#region Walls ______________________________________________________________________
+
+	private void PrepWalls ()
+	{
+		Scale_TopWall ();
+		Position_TopWall ();
+
+		Scale_BtmWall ();
+		Position_BtmWall ();
+
+		Scale_LeftWall ();
+		Position_LeftWall ();
+
+		Scale_RightWall ();
+		Position_RightWall ();
+	}
+
+	// Top
+
+	private void Scale_TopWall ()
+	{
+		topWall.localScale = new Vector3 (width * 2f, topWall.localScale.y, topWall.localScale.z);
+	}
+
+	private void Position_TopWall ()
+	{
+		topWall.localPosition = new Vector3 (topWall.localPosition.x, height / 2f, topWall.localPosition.z);
+	}
+
+	// Btm
+
+	private void Scale_BtmWall ()
+	{
+		btmWall.localScale = new Vector3 (width * 2f, btmWall.localScale.y, btmWall.localScale.z);
+	}
+
+	private void Position_BtmWall ()
+	{
+		btmWall.localPosition = new Vector3 (btmWall.localPosition.x, -(height / 2f + (1f / 32f * 12f)), btmWall.localPosition.z);
+	}
+
+	// Left
+
+	private void Scale_LeftWall ()
+	{
+		leftWall.localScale = new Vector3 (leftWall.localScale.x, height * 2f + (1f / 32f * 24f), leftWall.localScale.z);
+	}
+
+	private void Position_LeftWall ()
+	{
+		leftWall.localPosition = new Vector3 (-width / 2f, leftWall.localPosition.y, leftWall.localPosition.z);
+	}
+
+	// Right
+
+	private void Scale_RightWall ()
+	{
+		rightWall.localScale = new Vector3 (rightWall.localScale.x, height * 2f + (1f / 32f * 24f), rightWall.localScale.z);
+	}
+
+	private void Position_RightWall ()
+	{
+		rightWall.localPosition = new Vector3 (width / 2f, rightWall.localPosition.y, rightWall.localPosition.z);
+	}
+
+	#endregion
 }
